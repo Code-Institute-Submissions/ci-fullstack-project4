deeb63b5b504e3ad36773b9988a3d40a5454f5fa
Mien Ling <mien_ling@hotmail.com>
2020-08-08 09:37:26 +0000

    create models for product brand, product category, product country of origin, subcategory and product usage

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Brand, Category, Country, Subcategory, Usage
 
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(Subcategory)
+admin.site.register(Usage)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+
+
+class Brand(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    cat_no = models.IntegerField(blank=False)
+    url_tag = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Usage(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return self.name

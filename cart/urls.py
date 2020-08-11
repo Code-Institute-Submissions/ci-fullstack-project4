2683d452b6a9b5484935ca833cd038ec3b169383
Mien Ling <mien_ling@hotmail.com>
2020-08-11 00:06:38 +0000

    write the view function to add items to shopping cart, include the url and link up the cart image to the function

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+import cart.views
 
 urlpatterns = [
-    
+    path('add/<product_id>', cart.views.add_to_cart,
+         name="add_to_cart")
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (render, redirect, reverse,
+                              get_object_or_404)
 from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
-from products.model import Product
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('shopping_cart', {})
+    print(cart)
+    if product_id not in cart:
+        product = get_object_or_404(Product, pk=product_id)
+        cart[product_id] = {
+            'id': product_id,
+            'name': product.name,
+            'unit_cost': product.get_current_price(),
+            'qty': 1
+        }
+        """save the cart into the shopping cart session"""
+        request.session['shopping_cart'] = cart
+        messages.success(request, f'{product.name} been added to your cart!')
+        return redirect(reverse('products.views.index'))
+    else:
+        cart[product_id]['qty'] += 1
+        """save the cart into the shopping cart session again"""
+        request.session['shopping_cart'] = cart
+        return redirect(reverse('products.views.index'))
+
+

12b6cf5fa86d1af5659ae2c5dd7bebe13b90b0ff
Mien Ling <mien_ling@hotmail.com>
2020-08-16 16:35:00 +0000

    modify mykitchen view for register_household to add users to group to enable adding CRUD permissions, change migration file for mykitchen to create owner and member group and add permissions for each group

--- mykitchen/models.py
+++ mykitchen/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from products.models import Product
 import datetime
+
 # Create your models here.
 
 
--- mykitchen/views.py
+++ mykitchen/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .forms import HouseholdForm, MemberFormSet
 from .models import Member, Household
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group, User
 import datetime
 
 # Create your views here.
@@ -11,7 +12,6 @@ import datetime
 @login_required
 def index(request):
     # get user = request.user,
-    # get household id from Parameter
     user = request.user
 
     # check if logged in user is household member
@@ -26,13 +26,13 @@ def index(request):
     except Household.DoesNotExist:
         household = None
 
-    #if member exists, get household
+    # if member exists, get household
     if member:
         household = member.household
-    
+
     if household is None:
         household = ""
-        messages.success(
+        messages.info(
                 request,
                 "Please register the household and link the members."
         )
@@ -41,20 +41,39 @@ def index(request):
     })
 
 
+def view_household(request):
+    return render(request, 'mykitchen/view_household.template.html', {
+    })
+
+
 def register_household(request):
     if request.method == 'POST':
         house_form = HouseholdForm(request.POST)
         member_form = MemberFormSet(request.POST)
+
         # if the form is validated
         if house_form.is_valid():
-            household = house_form.save()
-            if member_form.isvalid():
-                members = member_form.save()
+            household = house_form.save(commit=False)
+            household.owner = request.user
+            owner_group = Group.objects.get(name='owner_group')
+            owner_group.user_set.add(household.owner)
+            if member_form.is_valid():
+                household.save()
+                members = member_form.save(commit=False)
+                for member in members:
+                    member_group = Group.objects.get(name='member_group')
+                    try:
+                        user = User.objects.get(username=member)
+                    except User.DoesNotExist:
+                        user = None
+                    member_group.user_set.add(user)
+                    member.household = household
+                    member.save()
                 messages.success(
-                        request,
-                        f"Your Household {household.name} profile"
-                        f" has been created on"
-                        f" {datetime.datetime.today().strftime('%b %d, %Y, %H:%M:%S')}")
+                    request,
+                    f"Your household profile {household.name}"
+                    f" has been created on"
+                    f" {datetime.datetime.today().strftime('%b %d, %Y, %H:%M:%S')}")
                 return redirect(reverse(index))
     house_form = HouseholdForm()
     member_form = MemberFormSet()

5bb6d712e5bfa5091c06ef82dbde6560505a5fe8
Mien Ling <mien_ling@hotmail.com>
2020-08-17 17:11:47 +0000

    modify register household route to prevent adding of user who are already members of other household, modify edit route to do the same check, to ensure members of other household are not added to the current user's household, add remove and adding of group membership for permission setting

--- mykitchen/urls.py
+++ mykitchen/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('', mykitchen.views.index,
          name="mykitchen_home_page"),
     path('register', mykitchen.views.register_household,
-         name="mykitchen_register")
+         name="register_household"),
+    path('view-household/<household_id>', mykitchen.views.view_household,
+         name="view_household"),
+    path('edit-household/<household_id>', mykitchen.views.edit_household,
+         name="edit_household"),
+    path('delete-household/<household_id>', mykitchen.views.delete_household,
+         name="delete_household")
 ]
--- mykitchen/views.py
+++ mykitchen/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .forms import HouseholdForm, MemberFormSet
 from .models import Member, Household
@@ -33,51 +33,133 @@ def index(request):
     if household is None:
         household = ""
         messages.info(
-                request,
-                "Please register the household and link the members."
+            request,
+            "Please register the household and link the members."
         )
     return render(request, 'mykitchen/mykitchen_index.template.html', {
         'household': household
     })
 
 
-def view_household(request):
+@login_required
+def view_household(request, household_id):
+    # if request.user = owner
+    household = Household.objects.get(id=household_id)
+    members = Member.objects.filter(household=household)
     return render(request, 'mykitchen/view_household.template.html', {
+        'household': household,
+        'members': members
     })
 
 
+@login_required
 def register_household(request):
     if request.method == 'POST':
         house_form = HouseholdForm(request.POST)
-        member_form = MemberFormSet(request.POST)
+        member_formset = MemberFormSet(request.POST, prefix="member")
 
         # if the form is validated
         if house_form.is_valid():
-            household = house_form.save(commit=False)
-            household.owner = request.user
+            house_form_instance = house_form.save(commit=False)
+            house_form_instance.owner = request.user
             owner_group = Group.objects.get(name='owner_group')
-            owner_group.user_set.add(household.owner)
-            if member_form.is_valid():
-                household.save()
-                members = member_form.save(commit=False)
-                for member in members:
+            owner_group.user_set.add(house_form_instance.owner)
+            if member_formset.is_valid():
+                members_form_instance = member_formset.save(commit=False)
+                house_form_instance.save()
+                for form_instance in members_form_instance:
                     member_group = Group.objects.get(name='member_group')
                     try:
-                        user = User.objects.get(username=member)
+                        user = User.objects.get(username=form_instance)
                     except User.DoesNotExist:
                         user = None
-                    member_group.user_set.add(user)
-                    member.household = household
-                    member.save()
+                    # check if user belongs to another household
+                    try:
+                        member = Member.objects.get(user=user)
+                    except Member.DoesNotExist:
+                        member = None
+
+                    if member is not None or user is None:
+                        messages.error(request,
+                                       f"{user}already belongs to another"
+                                       " household or No such User.")
+                        return redirect(reverse(index))
+                    else:
+                        # assign user to group
+                        member_group.user_set.add(user)
+                        # save form data
+                        form_instance.household = house_form_instance
+                        form_instance.save()
                 messages.success(
                     request,
-                    f"Your household profile {household.name}"
+                    f"Your household profile {house_form_instance.name}"
                     f" has been created on"
                     f" {datetime.datetime.today().strftime('%b %d, %Y, %H:%M:%S')}")
                 return redirect(reverse(index))
     house_form = HouseholdForm()
-    member_form = MemberFormSet()
+    member_formset = MemberFormSet(prefix="member")
     return render(request, 'mykitchen/register_household.template.html', {
         'house_form': house_form,
-        'member_form': member_form
+        'member_form': member_formset
     })
+
+
+def edit_household(request, household_id):
+    household_to_update = get_object_or_404(Household, pk=household_id)
+    current_members = Member.objects.filter(household=household_to_update)
+    household_members = [x.user for x in current_members]
+    if request.method == 'POST':
+        edit_house_form = HouseholdForm(request.POST,
+                                        instance=household_to_update)
+        edit_member_form = MemberFormSet(request.POST,
+                                         instance=household_to_update,
+                                         prefix="member")
+        if edit_house_form.is_valid():
+            edit_house_form.save(commit=False)
+            edit_house_form.owner = request.user
+            if edit_member_form.is_valid():
+                edit_member_form.save(commit=False)
+                edited_form_members = []
+                for new_member in edit_member_form.cleaned_data:
+                    if new_member.get('user'):
+                        edited_form_members.append(new_member.get('user'))
+                    else:
+                        pass
+                removed_members = [
+                    p for p in household_members if p not in
+                    edited_form_members]
+                added_members = [
+                    p for p in edited_form_members if p not in
+                    household_members]
+                check_for_membership = []
+                for member in added_members:
+                    try:
+                        member = Member.objects.get(user=member)
+                    except Member.DoesNotExist:
+                        member = None
+                    check_for_membership.append(member)
+                if all(x is None for x in check_for_membership):
+                    member_group = Group.objects.get(name='member_group')
+                    member_group.user_set.remove(*removed_members)
+                    member_group.user_set.add(*added_members)
+                    # save the household object
+                    edit_house_form.save()
+                    # save the members
+                    edit_member_form.save()
+                    household_name = edit_house_form.cleaned_data["name"]
+                    messages.success(
+                        request,
+                        f"Your household profile {household_name}"
+                        f" has been edited on"
+                        f" {datetime.datetime.today().strftime('%b %d, %Y, %H:%M:%S')}")
+                    return redirect(reverse(index))
+    edit_house_form = HouseholdForm(instance=household_to_update)
+    edit_member_form = MemberFormSet(instance=household_to_update,
+                                     prefix="member")
+    return render(request, 'mykitchen/update_household.template.html', {
+                  'house_form': edit_house_form,
+                  'member_form': edit_member_form
+                  })
+
+def delete_household(request, household_id):
+    pass

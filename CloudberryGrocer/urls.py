2f553f1f91e0685134dde696aaeb158c55f660d0
Mien Ling <mien_ling@hotmail.com>
2020-08-23 07:13:11 +0000

    show discounted price next to original price in product details, category template, add homepage url

--- CloudberryGrocer/urls.py
+++ CloudberryGrocer/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', products.views.index, name='home_page_route'),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include("checkout.urls")),
